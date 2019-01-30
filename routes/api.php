b77595c1c1ae71aa3f55a13052d41cac1a9b6aae
Renato Torres <nari9rock@hotmail.com>
2019-01-30 00:45:30 -0200

    criando cors

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group([
-    'middleware' => 'api',
+    'middleware' => 'cors',
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', 'AuthController@login');
