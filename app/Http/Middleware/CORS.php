e3381a1a5d6b41679b76e16da127c7df0bd1a4ed
Renato Torres <nari9rock@hotmail.com>
2019-02-01 21:11:55 -0200

    ajustando cors para aceitar o token JWT

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -17,7 +17,7 @@ class CORS {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
     }
 
 }
